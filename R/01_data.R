336610bc1079874ffbdd3012505b6e593984dbea
lotmta <lotmateohernandezespinosa@gmail.com>
2023-02-11 12:52:44 -0600

    Quito algunos dobles enter's entre lineas porque se ve feo

--- R/01_data.R
+++ R/01_data.R
@@ -1,10 +1,8 @@
 # Cargo recount3
 library("recount3")
 
-
 # Cambio la url
 options(recount3_url = "https://recount-opendata.s3.amazonaws.com/recount3/release")
-
 
 # Cargo los datos del estudio, en este caso SRP133502 ( scRNAseq of aging dermal fibroblasts )
 proj_info <- subset(
@@ -12,10 +10,8 @@ proj_info <- subset(
     project == "SRP133502" & project_type == "data_sources"
 )
 
-
 # Se crea el rse
 rse_gene_SRP133502 = create_rse(proj_info)
-
 
 # Convierto las cuentas por nucleotido a cuentas por lectura
 assay(rse_gene_SRP133502, "counts") <- compute_read_counts(rse_gene_SRP133502)
