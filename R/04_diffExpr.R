a490b35bad0620e6ab74e860412deb9b60229d52
lotmta <lotmateohernandezespinosa@gmail.com>
2023-02-12 20:30:12 -0600

    Añado variable al rse para los 9 grupos

--- R/04_diffExpr.R
+++ R/04_diffExpr.R
@@ -4,3 +4,6 @@ library("ggplot2")
 
 # Cargamos nuestro rse
 load('processed-data/rse_gene_SRP199678.RData')
+
+# Creamos los 9 grupos
+rse_gene_SRP199678$categoria <- paste(rse_gene_SRP199678$sra_attribute.age, rse_gene_SRP199678$sra_attribute.cx3cr1_genotype)
